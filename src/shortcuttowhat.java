bea4d2c89e4fefb408288119829c1196ac09d806
mforbeskr <mforbeskr@outlook.dk>
2026-08-13 21:33:33 +0200

    cat piss

---
+++ src/shortcuttowhat.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class shortcuttowhat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int output = ((sc.nextInt() + 5) * 3 - 10);
+
+        System.out.println(output);
+    }
+}
